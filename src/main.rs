19078b6418555fc210938ca64d6ce7991f3813b7
speedcell4 <speedcell4@gmail.com>
2021-09-10 22:15:15 +0900

    Feat: Add collect_offsets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::fs::File;
+use std::io;
+use std::io::Read;
+use std::path::Path;
+
+use rand::prelude::SliceRandom;
+use rand::thread_rng;
+
+fn collect_offsets(path: &Path) -> Result<Vec<u64>, io::Error> {
+    let mut offsets: Vec<_> = File::open(path)?.bytes()
+        .zip(0u64..)
+        .filter_map(|(byte, index)| {
+            if let Ok(b) = byte {
+                if b == b'\n' {
+                    return Some(index + 1);
+                }
+            }
+            None
+        })
+        .collect();
+    offsets.push(0);
+
+    Ok(offsets)
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let offsets = collect_offsets(
+        Path::new("data/example1.txt")
+    );
+
+
+    println!("offsets => {:?}", offsets);
 }
