46fa44634a77e30e9a68c626d65d474153338e4a
speedcell4 <speedcell4@gmail.com>
2021-09-10 23:04:10 +0900

    Feat: Add dump_offsets and load_offsets

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::io;
-use std::io::Read;
+use std::io::{Error, Read, Write};
+use std::mem::transmute;
+use std::panic::panic_any;
 use std::path::Path;
 
 use rand::prelude::SliceRandom;
@@ -23,12 +25,49 @@ fn collect_offsets(path: &Path) -> Result<Vec<u64>, io::Error> {
     Ok(offsets)
 }
 
+fn dump_offsets(offsets: &Vec<u64>, path: &Path) -> Result<(), io::Error> {
+    let mut writer = OpenOptions::new()
+        .write(true)
+        .open(path)?;
 
-fn main() {
+    for &offset in offsets {
+        let bytes = unsafe { transmute::<u64, [u8; 8]>(offset) };
+        writer.write(&bytes)?;
+    }
+
+    Ok(())
+}
+
+fn load_offsets(path: &Path) -> Result<Vec<u64>, io::Error> {
+    let mut reader = OpenOptions::new()
+        .read(true)
+        .open(path)?;
+
+    let mut ret: Vec<u64> = Vec::new();
+    let mut buf: [u8; 8] = [0; 8];
+
+    while let Ok(..) = reader.read_exact(&mut buf) {
+        let offset = unsafe {
+            transmute::<[u8; 8], u64>(buf)
+        };
+        ret.push(offset);
+    }
+
+    Ok(ret)
+}
+
+fn main() -> Result<(), Error> {
     let offsets = collect_offsets(
         Path::new("data/example1.txt")
-    );
+    )?;
 
+
+    dump_offsets(&offsets, Path::new("data/nice.txt"))?;
 
     println!("offsets => {:?}", offsets);
+
+    let miao = load_offsets(Path::new("data/nice.txt"))?;
+    println!("miao => {:?}", miao);
+
+    Ok(())
 }
