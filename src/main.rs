ce9884adcb06ebe71ebb6baaf0ce44165254ad83
speedcell4 <speedcell4@gmail.com>
2021-09-11 00:51:51 +0900

    Refactor: Introduce FilesShuffler

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use std::collections::HashMap;
-use std::error::Error;
 use std::fs::{File, OpenOptions};
 use std::io;
-use std::io::{BufRead, BufReader, BufWriter, Read, Seek, SeekFrom, Write};
-use std::mem::transmute;
+use std::io::{BufRead, BufReader, BufWriter, Seek, SeekFrom, Write};
 use std::path::{Path, PathBuf};
 
 use rand::{Rng, thread_rng};
 use rand::prelude::SliceRandom;
+use rand::rngs::ThreadRng;
 
-use offset::{Offset, Offsets};
+use offset::Offsets;
 
 mod offset;
 
@@ -40,59 +39,72 @@ impl LineWriter {
     }
 }
 
-fn build_offsets<'a>(paths: &Vec<&'a Path>) -> HashMap<&'a Path, PathBuf> {
-    let mut rng = thread_rng();
-
-    paths.iter().map(|&path| {
-        let mut offsets = offset::collect_offsets(path).unwrap();
-        offsets.shuffle(&mut rng);
-
-        let offset_path = path.with_extension(".offsets").to_owned();
-        offset::dump_offsets(&offsets, offset_path.as_path());
-
-        (path, offset_path)
-    }).collect()
+struct FilesShuffler {
+    rng: ThreadRng,
+    line_writer: LineWriter,
 }
 
-fn shuffle_files(map: &HashMap<&Path, PathBuf>, paths: &Vec<&Path>, size: usize) {
-    let mut rng = thread_rng();
-    let mut copy_line = LineWriter::new();
+impl FilesShuffler {
+    fn new() -> FilesShuffler {
+        FilesShuffler {
+            rng: thread_rng(),
+            line_writer: LineWriter::new(),
+        }
+    }
 
-    let mut iter = map.iter();
-    let mut readers: Vec<_> = (0..size).map(|_| iter.next().unwrap()).map(|(path, offset_path)| {
-        let file = OpenOptions::new().read(true).open(path).unwrap();
-        let offsets = offset::load_offsets(offset_path.as_path()).unwrap();
-        (BufReader::new(file), offsets)
-    }).collect();
+    fn build_offsets<'a>(&mut self, src_paths: &Vec<&'a Path>) -> HashMap<&'a Path, PathBuf> {
+        src_paths.iter().map(|&src_path| {
+            let mut offsets = offset::collect_offsets(src_path).unwrap();
+            offsets.shuffle(&mut self.rng);
 
-    let mut writers: Vec<_> = paths.iter().map(|path| {
-        let file = OpenOptions::new().create(true).write(true).open(path).unwrap();
-        BufWriter::new(file)
-    }).collect();
+            let offset_path = src_path.with_extension(".offsets").to_owned();
+            offset::dump_offsets(&offsets, offset_path.as_path()).unwrap();
 
-    while !readers.is_empty() {
-        let index1 = rng.gen_range(0..readers.len());
-        let index2 = rng.gen_range(0..writers.len());
+            (src_path, offset_path)
+        }).collect()
+    }
 
-        let (reader, offsets) = readers.get_mut(index1).unwrap();
-        let writer = writers.get_mut(index2).unwrap();
+    fn shuffle_files(&mut self, map: &HashMap<&Path, PathBuf>, paths: &Vec<&Path>, size: usize) -> () {
+        let mut iter = map.iter();
+        let mut readers: Vec<_> = (0..size).map(|_| iter.next().unwrap()).map(|(path, offset_path)| {
+            let file = OpenOptions::new().read(true).open(path).unwrap();
+            let offsets = offset::load_offsets(offset_path.as_path()).unwrap();
+            (BufReader::new(file), offsets)
+        }).collect();
 
-        copy_line.write_line(offsets, reader, writer);
-        if offsets.len() == 0 {
-            readers.remove(index1);
-            if let Some((path, offset_path)) = iter.next() {
-                let file = OpenOptions::new().read(true).open(path).unwrap();
-                let offsets = offset::load_offsets(offset_path.as_path()).unwrap();
-                readers.push((BufReader::new(file), offsets));
+        let mut writers: Vec<_> = paths.iter().map(|path| {
+            let file = OpenOptions::new().create(true).write(true).open(path).unwrap();
+            BufWriter::new(file)
+        }).collect();
+
+        while !readers.is_empty() {
+            let index1 = self.rng.gen_range(0..readers.len());
+            let index2 = self.rng.gen_range(0..writers.len());
+
+            let (reader, offsets) = readers.get_mut(index1).unwrap();
+            let writer = writers.get_mut(index2).unwrap();
+
+            self.line_writer.write_line(offsets, reader, writer).unwrap();
+            if offsets.len() == 0 {
+                readers.remove(index1);
+                if let Some((path, offset_path)) = iter.next() {
+                    let file = OpenOptions::new().read(true).open(path).unwrap();
+                    let offsets = offset::load_offsets(offset_path.as_path()).unwrap();
+                    readers.push((BufReader::new(file), offsets));
+                }
             }
         }
     }
 }
 
+
 fn main() {
     let path1 = vec![Path::new("data/example1.txt")];
     let path2 = vec![Path::new("data/example1.out.txt")];
 
-    let map = build_offsets(&path1);
-    shuffle_files(&map, &path2, 1);
+    let mut files_shuffler = FilesShuffler::new();
+
+
+    let map = files_shuffler.build_offsets(&path1);
+    files_shuffler.shuffle_files(&map, &path2, 1);
 }
